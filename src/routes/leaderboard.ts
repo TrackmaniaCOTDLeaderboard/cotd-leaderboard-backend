aa17676a202b7b25d4a44ff646a8b28b632947e5
Markus Wieland <wie.markus@web.de>
2024-06-29 08:41:22 +0200

    added leaderboard entpoints

--- src/routes/leaderboard.ts
+++ src/routes/leaderboard.ts
@@ -5,7 +5,13 @@ const router = express.Router();
 
 router.get("/cup/global", LeaderboardController.getGlobalCupLeaderboard);
 router.get("/cup/monthly/:year/:month", LeaderboardController.getMonthlyCupLeaderboard);
+
 router.get("/time-attack/global", LeaderboardController.getGlobalTimeAttackLeaderboard);
+router.get("/time-attack/monthly/:year/:month", LeaderboardController.getMonthlyTimeAttackLeaderboard);
+
 router.get("/challenge/global", LeaderboardController.getGlobalChallengeLeaderboard);
+router.get("/challenge/monthly/:year/:month", LeaderboardController.getMonthlyChallengeLeaderboard);
+
+router.get("/mapper", LeaderboardController.getMapperLeaderboard);
 
 export default router;
