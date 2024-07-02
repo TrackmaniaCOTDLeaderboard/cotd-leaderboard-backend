85e5e2fba6f5342b11fea926f521240fbd6c1feb
Markus Wieland <wie.markus@web.de>
2024-07-02 12:44:43 +0200

    added documentation

--- src/util/assert-defined.ts
+++ src/util/assert-defined.ts
@@ -1,3 +1,20 @@
+/** Asserts that a value is defined (i.e., not null or undefined).
+ *
+ * This function is a type guard that throws an error if the provided value is null or undefined.
+ * It helps ensure that the value is non-nullable in subsequent code.
+ *
+ * @template T - The type of the value to be checked.
+ * @param val - The value to be checked for being defined.
+ * @param [name="variable"] - The name of the variable for the error message (optional).
+ * @throws Throws an error if the value is null or undefined.
+ *
+ * @example
+ * const maybeValue: string | undefined = getValue();
+ * assertIsDefined(maybeValue, 'maybeValue');
+ * // From this point, TypeScript knows that `maybeValue` is not null or undefined
+ * console.log(maybeValue.toUpperCase());
+ */
+
 export function assertIsDefined<T>(val: T, name = "variable"): asserts val is NonNullable<T> {
     if (!val) {
         throw Error(`Expected ${name} to be defined, but received ${val}`);
--- src/util/wait.ts
+++ src/util/wait.ts
@@ -1,3 +1,15 @@
+/**
+ * Pauses the execution for a specified number of seconds.
+ *
+ * This function returns a promise that resolves after the specified number of seconds.
+ *
+ * @param seconds - The number of seconds to wait.
+ * @returns A promise that resolves after the specified number of seconds.
+ *
+ * @example
+ * // Wait for 2 seconds
+ * await wait(2);
+ */
 export const wait = async (seconds: number): Promise<void> => {
     return new Promise(resolve => {
         setTimeout(resolve, seconds * 1000);
